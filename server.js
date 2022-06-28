be0dbd6f9769ed87b472867243e5b5723b5651a6
jesse sheehan <jessetsheehan@gmail.com>
2022-06-28 18:09:36 -0400

    added front end interactions with back end for client

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
-
+//parses routes to find other linked data (like CSS and js files for front end - the 'public' is a reference to the  folder/directory with the same name, and thus, everything in it)
+app.use(express.static('public'));
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
@@ -103,6 +104,22 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
    }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.post('/api/animals', (req, res) => {
     //req.body is where our incoming content will be
