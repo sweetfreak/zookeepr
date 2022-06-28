68fb1eeaecbc0cbad5c5a5d2d8316f2e23b1dbb2
jesse sheehan <jessetsheehan@gmail.com>
2022-06-28 09:32:36 -0400

    added functionality to add animals to json and validate the post

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+//this makes it so the data sent via a POST is readable by the server - see 11.2.5
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 const {animals} = require('./data/animals');
 
@@ -21,6 +29,20 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    //_dirname = director of file we execute code in, with path of the following file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'), JSON.stringify({animals: animalsArray }, null, 2)
+    );
+
+
+    //return finished code to post route for response
+    return animal;
+}
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
    if (result) {
@@ -28,6 +50,22 @@ app.get('/api/animals/:id', (req, res) => {
    } else {
     res.send(404);
    }
+});
+
+app.post('/api/animals', (req, res) => {
+    //req.body is where our incoming content will be
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted')
+    } else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
 })
 
 app.listen(PORT, () => {
@@ -69,4 +107,20 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name)
     }
     return filteredResults;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 }
